bdbe062dfb51540048c2517cfb9334f53b195328
Musaddek Ali <mussali.one@gmail.com>
2024-04-15 19:11:02 +0000

    cp

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -2,13 +2,13 @@ function userInformationHTML(user) {
     return `
         <h2>${user.name}
             <span class="small-name">
-                (@<a href="${user.html_url}" target="_blank">${user.login}</a>
+                (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
             </span>
         </h2>
         <div class="gh-content">
             <div class="gh-avatar">
-                <a href="${user.html_url} target="_blank">
-                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" /> 
+                <a href="${user.html_url}" target="_blank">
+                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
                 </a>
             </div>
             <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
@@ -22,18 +22,18 @@ function repoInformationHTML(repos) {
 
     var listItemsHTML = repos.map(function(repo) {
         return `<li>
-            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
-        </li>`;
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`;
     });
 
     return `<div class="clearfix repo-list">
-        <p>
-            <strong>Repo List:</strong>
-        </p>
-        <ul>
-            ${listItemsHTML.join("\n")}
-        </ul>
-    </div>`;
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}
+                </ul>
+            </div>`;
 }
 
 function fetchGitHubInformation(event) {
